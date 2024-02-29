87474b50bcaeea1b45fa8b23ecd6120d04a998f4
Nayara <queiroz.14@hotmail.com>
2024-02-29 16:32:48 -0300

    DOM exercises

---
+++ dom/script.js
@@ -0,0 +1,13 @@
+// Retorne o url da página atual utilizando o objeto window
+const url = window.location.href;
+
+console.log(url);
+
+// Seleciona o primeiro elemento da página que possua a classe ativo
+const firstElement = document.querySelector('.ativo');
+
+console.log(firstElement);
+// Retorne a linguagem do navegador
+console.log(window.navigator.language);
+// Retorne largura da janela
+console.log(window.innerWidth);
